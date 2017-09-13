b54354ba8c718921ef162698df6cfa0d877979b3
juaninch <juaninch@juaninch.com>
2017-09-14 01:06:04 +0900

    slight refactoring

--- batbaby/batbaby.py
+++ batbaby/batbaby.py
@@ -7,13 +7,15 @@ url = "https://en.wikipedia.org/wiki/List_of_Batman_children's_books"
 
 r = requests.get(url)
 soup = BeautifulSoup(r.content, 'lxml')
-table = soup.find("table", attrs={'class': 'wikitable'}).find_all('tr', attrs={'style':'text-align: center; background:#F2F2F2;'})
-books = []
+table = soup.find("table", attrs={'class': 'wikitable'})
+table_rows = table.find_all('tr', attrs={'style':'text-align: center; background:#F2F2F2;'})
+
 Book = namedtuple('Book', ['title', 'author'])
-for row in table:
-    title = row.find('td', attrs={'style': 'text-align: left;'})
+books = []
+for row in table_rows:
+    title = row.find('td', attrs={'style': 'text-align: left;'}).string
     author = title.findNext('td').string
-    books.append(Book(title=title.string, author=author.string))
+    books.append(Book(title=title, author=author))
 
 # need to get ratings from somewhere
 
