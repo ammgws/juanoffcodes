0dec6d74c0d1879fdfcbc36f834a9fb44c68861b
juaninch <juaninch@juaninch.com>
2017-09-14 01:23:10 +0900

    get ratings (WIP)

--- batbaby/batbaby.py
+++ batbaby/batbaby.py
@@ -1,22 +1,35 @@
 from collections import namedtuple
 
+import click
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://en.wikipedia.org/wiki/List_of_Batman_children's_books"
+@click.command()
+@click.option('--key', '-k',
+              type=click.STRING,
+              help='Goodreads dev API key',
+             )
+def main(key):
+    url = "https://en.wikipedia.org/wiki/List_of_Batman_children's_books"
+    ratings_url = f"https://www.goodreads.com/book/review_counts.json?key={key}&isbns="
 
-r = requests.get(url)
-soup = BeautifulSoup(r.content, 'lxml')
-table = soup.find("table", attrs={'class': 'wikitable'})
-table_rows = table.find_all('tr', attrs={'style':'text-align: center; background:#F2F2F2;'})
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'lxml')
+    table = soup.find("table", attrs={'class': 'wikitable'})
+    table_rows = table.find_all('tr', attrs={'style':'text-align: center; background:#F2F2F2;'})
 
-Book = namedtuple('Book', ['title', 'author'])
-books = []
-for row in table_rows:
-    title = row.find('td', attrs={'style': 'text-align: left;'}).string
-    author = title.findNext('td').string
-    books.append(Book(title=title, author=author))
+    Book = namedtuple('Book', ['title', 'author'])
+    books = []
+    for row in table_rows:
+        title = row.find('td', attrs={'style': 'text-align: left;'}).string
+        author = title.findNext('td').string
 
-# need to get ratings from somewhere
+        books.append(Book(title=title, author=author))
 
-print(books)
+    # can get ratings from goodreads but need ISBNs
+
+    print(books)
+
+if __name__ == "__main__":
+    main()
+
