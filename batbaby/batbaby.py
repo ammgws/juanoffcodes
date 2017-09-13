402a4351dcb4692cace9332ec03dd122e5efbd3a
juaninch <juaninch@juaninch.com>
2017-09-14 01:39:56 +0900

    get ratings from goodreads

--- batbaby/batbaby.py
+++ batbaby/batbaby.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+from time import sleep
 
 import click
 import requests
@@ -12,21 +13,26 @@ from bs4 import BeautifulSoup
 def main(key):
     url = "https://en.wikipedia.org/wiki/List_of_Batman_children's_books"
     ratings_url = f"https://www.goodreads.com/book/review_counts.json?key={key}&isbns="
-
     r = requests.get(url)
     soup = BeautifulSoup(r.content, 'lxml')
     table = soup.find("table", attrs={'class': 'wikitable'})
     table_rows = table.find_all('tr', attrs={'style':'text-align: center; background:#F2F2F2;'})
 
-    Book = namedtuple('Book', ['title', 'author'])
+    Book = namedtuple('Book', ['title', 'author', 'rating'])
     books = []
-    for row in table_rows:
-        title = row.find('td', attrs={'style': 'text-align: left;'}).string
-        author = title.findNext('td').string
 
-        books.append(Book(title=title, author=author))
+    with click.progressbar(table_rows) as rows:
+        for row in rows:
+            title = row.find('td', attrs={'style': 'text-align: left;'}).string
+            author = title.findNext('td').string
 
-    # can get ratings from goodreads but need ISBNs
+            gurl = f"https://www.goodreads.com/search/index.xml?key={key}&q={title} {author}"
+            r = requests.get(gurl)
+            soup = BeautifulSoup(r.content, 'lxml')
+            rating = soup.find('average_rating').string
+
+            books.append(Book(title=title, author=author, rating=rating))
+            sleep(1)  # to abide by Goodreads' API limits
 
     print(books)
 
